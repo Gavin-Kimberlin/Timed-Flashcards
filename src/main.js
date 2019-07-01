dad5640f896955f90cb55436bea0ee7f752f11b3
Ben Martinson and Gavin Kimberlin <ben@>
2019-07-01 15:17:44 -0700

    add css styling and get bootstrap working.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 import FlashCards from './../src/timed-flashcards.js';
 
 
--- src/timed-flashcards.js
+++ src/timed-flashcards.js
@@ -29,28 +29,3 @@ export default class FlashCards {
   }
 
 };
-
-
-
-
-
-// print(cardInfo, 3000);
-// setInterval(function(){
-//   print(cardInfo, 3000);
-// }, cardInfo.length * 6000);
-// print(cardTitle,0);
-// setInterval(function(){
-//   print(cardTitle, 0);
-// }, cardInfo.length * 6000);
-
-// cardTitle.forEach(function (title) {
-//   setInterval (function(){
-//     console.log(title);
-//   }, 3000)
-// })
-
-// doSetTimeout(i, n){
-//   setTimeout(function(){
-//     console.log(i);
-//   }, n*3000)
-// }
